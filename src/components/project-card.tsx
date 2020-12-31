12f174daba7adcaccf4b3579ca84a830944e6a60
Omer Gulen <omergulen@jotform.com>
2020-12-31 23:04:32 +0300

    ProjectCard :: fix position

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -30,7 +30,7 @@ const ProjectCard = ({ link, title, children, bgImage, bgStartColor, bgEndColor 
       background: bg,
       transition: `all 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275) !important`,
       backgroundRepeat: `no-repeat`,
-      backgroundPositionX: `120%`,
+      backgroundPositionX: `110%`,
       backgroundPositionY: `center`,
       backgroundBlendMode: `luminosity`,
       backgroundSize: `contain`,
